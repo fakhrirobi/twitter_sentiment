111f38d787d7307dad39cafade5ff45e316685bf
fakhrirobi <60530014+fakhrirobi@users.noreply.github.com>
2021-02-19 00:22:57 +0700

    Update loot_data.py

--- loot_data.py
+++ loot_data.py
@@ -6,16 +6,16 @@ import emoji
 
 
 def load_data(dbname='fakhrirobi.db') : 
-    bearer_token = r"AAAAAAAAAAAAAAAAAAAAAG3DHQEAAAAAVosgDgIlxnpsLv5eFWE%2FZ95BTh4%3DFtNx0xVrGdFfMfLXuxvod0z6BTxJDKb5QlGvstckU3Ldl0VDHF"
-    consumer_key = "4gHwvfEYta9U0G9FhBYXHmj4b"
-    consumer_secret = "oyskJlBwdb6Ao39pkLAdILV1lvrbjWCVIIe985P37fPq8idTm2"
-    access_token = "1302508617124388865-yzaO7XDdIlYCAonhkBjahCyFMx1zXQ"
-    access_token_secret = "IINTcNV5L6xzm0UtY3Rh920NMr0KUebq0HiwXRpohwzgD"
+    bearer_token = "get the API key first by signing up dev account on twitter"
+    consumer_key = "get the API key first by signing up dev account on twitter"
+    consumer_secret = "get the API key first by signing up dev account on twitter"
+    access_token = "get the API key first by signing up dev account on twitter"
+    access_token_secret = "get the API key first by signing up dev account on twitter"
     auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
     auth.set_access_token(access_token, access_token_secret)
     api = tweepy.API(auth,wait_on_rate_limit=True)
 
-    jangka_waktu = 7
+    date_range = 7
     end_date = datetime.now()
     end_date = end_date.strftime('%d')
     final_date = datetime.now()
@@ -33,13 +33,14 @@ def load_data(dbname='fakhrirobi.db') :
     for count,tweet in enumerate(twitter_query) :
         tweet_text =  tweet.full_text
         tweet_text = tweet.full_text.casefold()
-        tanggal = datetime.date(tweet.created_at)
-        def hapus_tanda(tweet): 
+        date = datetime.date(tweet.created_at)
+        
+        def delete_punc(tweet): 
             tanda_baca = set(string.punctuation)
             tweet = ''.join(ch for ch in tweet if ch not in tanda_baca)
             return tweet
         
-        def hapus_katadouble(tweet_text): 
+        def delete_double_words(tweet_text): 
             pattern = re.compile(r"(.)\1{1,}", re.DOTALL)
             return pattern.sub(r"\1\1", tweet_text)
 
@@ -51,18 +52,18 @@ def load_data(dbname='fakhrirobi.db') :
         #removing tag 
         tweet_text = re.sub(r'#([^\s]+)', r'\1', tweet_text)
         #removing punctuation
-        tweet_text=hapus_tanda(tweet_text)
+        tweet_text=delete_punc(tweet_text)
         #removing all the number
         tweet_text=re.sub(r'\w*\d\w*', '',tweet_text).strip()
         # removing double char
-        tweet_text=hapus_katadouble(tweet_text)
+        tweet_text=delete_double_words(tweet_text)
         tweet_text = re.sub(r'\n','',tweet_text)
         tweet_text = emoji.demojize(tweet_text)
         tweet_text = tweet_text.replace(":"," ")
         tweet_text = ' '.join(tweet_text.split())
         
 
-        content.append([tweet.id,'@%s'%(tweet.user.screen_name),tanggal,tweet_text]) 
+        content.append([tweet.id,'@%s'%(tweet.user.screen_name),date,tweet_text]) 
 
 
     import sqlite3 
@@ -73,13 +74,13 @@ def load_data(dbname='fakhrirobi.db') :
     table_query  = ''' CREATE TABLE IF NOT EXISTS twitter_data( id INTEGER PRIMARY KEY AUTOINCREMENT,
                                                                 twitter_id TEXT NOT NULL,
                                                                 username TEXT NOT NULL,
-                                                                tanggal TIMESTAMP NOT NULL,
+                                                                date TIMESTAMP NOT NULL,
                                                                 tweet TEXT NOT NULL,
                                                                 sentiment_label integer  ) '''
     cursor.execute(table_query)
 
     add_data = ''' INSERT INTO twitter_data
-                    (twitter_id,username,tanggal,tweet) VALUES (?,?,?,?)'''
+                    (twitter_id,username,date,tweet) VALUES (?,?,?,?)'''
 
     for i in range(0,len(content)) : 
         a,b,c,d = content[i]
