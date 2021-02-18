965811468179b4928d68986ee6008b2881c3c692
fakhrirobi <60530014+fakhrirobi@users.noreply.github.com>
2021-02-19 00:17:00 +0700

    Update visualize.py

--- visualize.py
+++ visualize.py
@@ -4,15 +4,17 @@ import matplotlib.pyplot as plt
 import numpy as np 
 
 def visualize_data(dbname='fakhrirobi.db') :
-    start_date = input('tanggal awal format 2020-09-17\n')
-    end_date = input('tanggal akhir format 2020-09-17\n')
+    #adding start date and end date for tweet
+    start_date = input('input start date, format 2020-09-17\n')
+    end_date = input('input end date,  format 2020-09-17\n')
     conn = sqlite3.connect(dbname)
     cursor = conn.cursor()
     query = ''' SELECT *  FROM twitter_data WHERE tanggal  BETWEEN ? AND ? '''
     cursor.execute(query,(start_date,end_date))
-
+    #fething tweet based on desired start and end date 
     view = cursor.fetchall()
     view
+    #printing the score of sentiment
     print('nilai rata rata untuk sentimen vaksin covid-19 adalah \n'+ str(np.mean(data_view['sentiment'])))
     print('nilai median  untuk sentimen vaksin covid-19 adalah \n'+ str(np.median(data_view['sentiment'])))
     print('nilai standar deviasi untuk sentimen vaksin covid-19 adalah \n'+ str(np.std(data_view['sentiment'])))
